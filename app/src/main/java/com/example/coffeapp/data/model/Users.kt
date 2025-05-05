7b707c245e3cce132fd9f1ed634b13d0df1fc5e4
Irem tufekci <113184262+iremtufekci35@users.noreply.github.com>
2025-05-05 16:06:57 +0300

    remote database migration

--- app/src/main/java/com/example/coffeapp/data/model/Users.kt
+++ app/src/main/java/com/example/coffeapp/data/model/Users.kt
@@ -3,10 +3,10 @@ package com.example.coffeapp.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "coffees")
-data class Coffee(
+@Entity(tableName = "users")
+data class Users(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val name: String,
-    val price: Double,
-    val imageUrl: String
+    val username: String,
+    val email: String,
+    val password: String
 )
